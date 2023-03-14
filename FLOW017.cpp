b5b162502fa8b32f954f6ad5c2ccf75f3674bdf4
Ritik Sharma <106161645+128hritik@users.noreply.github.com>
2023-03-15 01:05:34 +0530

    Add files via upload

---
+++ FLOW017.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   int t;
+   cin>>t;
+   while(t--)
+   {
+       
+     int a,b,c;
+        cin>>a>>b>>c;
+        if((a>b && a<c) || (a<b && a>c)){
+            cout<<a<<endl;
+        }else if((b>a && b<c) || (b<a && b>c)){
+            cout<<b<<endl;
+        }else if((c>a && c<b)|| (c<a && c>b)){
+            cout<<c<<endl;
+        }
+   }
+    
+	return 0;
+}
